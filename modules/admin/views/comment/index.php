98fe19c812a6ea3d4a8b6de747d20af63919e075
Kostiantyn Logunov <777koc@gmail.com>
2018-03-14 01:16:39 +0200

    first commit

---
+++ modules/admin/views/comment/index.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\CategorySearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Comments';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="category-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?php if (!empty($comments)):?>
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Author</th>
+                <th>Text</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+
+            <tbody>
+                <?php foreach ($comments as $comment) :?>
+                    <tr>
+                        <td><?= $comment->id; ?></td>
+                        <td><?= $comment->user->name; ?></td>
+                        <td><?= $comment->text; ?></td>
+                        <td>
+                            <?php if ($comment->isAllowed()):?>
+                                <a class="btn btn-warning" href="<?= Url::toRoute(['comment/disallow', 'id'=>$comment->id]); ?>">Disallow</a>
+                            <?php else:?>
+                                <a class="btn btn-success" href="<?= Url::toRoute(['comment/allow', 'id'=>$comment->id]); ?>">Allow</a>
+                            <?php endif;?>
+
+                            <a class="btn btn-danger" href="<?= Url::toRoute(['comment/delete', 'id'=>$comment->id]); ?>">Delete</a>
+                        </td>
+                    </tr>
+                <?php endforeach;?>
+            </tbody>
+        </table>
+    <?php endif;?>
+
+</div>
